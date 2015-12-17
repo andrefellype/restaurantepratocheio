3b12a151f1ac82d3346488fac75c9ebd60060922
ALUNO 2015-1 <ALUNO 2015-1@JAN-LB7-COM-030>
2015-12-17 09:10:47 -0200

    Correções do Cadastro de Produtos

--- src/br/edu/ifnmg/rpc/apresentacao/CadProdutoForm.java
+++ src/br/edu/ifnmg/rpc/apresentacao/CadProdutoForm.java
@@ -11,6 +11,8 @@ public class CadProdutoForm extends javax.swing.JInternalFrame {
         initComponents();
     }
 
+
+
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -21,6 +23,8 @@ public class CadProdutoForm extends javax.swing.JInternalFrame {
         lblLogin = new javax.swing.JLabel();
         btnCancelar = new javax.swing.JButton();
         btnSalvar = new javax.swing.JButton();
+        cmbTipo = new javax.swing.JComboBox();
+        lblNome1 = new javax.swing.JLabel();
 
         setClosable(true);
         setMaximizable(true);
@@ -49,46 +53,57 @@ public class CadProdutoForm extends javax.swing.JInternalFrame {
             }
         });
 
+        cmbTipo.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Selecione um Opção", "Bebida", "Comida a KG", "PF" }));
+
+        lblNome1.setFont(new java.awt.Font("Times New Roman", 0, 12)); // NOI18N
+        lblNome1.setText("Tipo:");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(btnCancelar)
+                .addGap(28, 28, 28)
+                .addComponent(btnSalvar, javax.swing.GroupLayout.PREFERRED_SIZE, 114, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(58, 58, 58))
             .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(txtNome)
+                    .addComponent(txtValor)
+                    .addComponent(cmbTipo, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(txtNome)
-                            .addComponent(txtValor)
                             .addGroup(layout.createSequentialGroup()
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(lblNome, javax.swing.GroupLayout.PREFERRED_SIZE, 97, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(lblLogin))
-                                .addGap(0, 0, Short.MAX_VALUE))))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(39, 39, 39)
-                        .addComponent(btnCancelar)
-                        .addGap(32, 32, 32)
-                        .addComponent(btnSalvar, javax.swing.GroupLayout.PREFERRED_SIZE, 114, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(0, 53, Short.MAX_VALUE)))
+                                .addComponent(lblLogin, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addGap(68, 68, 68))
+                            .addComponent(lblNome, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(lblNome1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addGap(248, 248, 248)))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(lblNome)
+                .addGap(18, 18, 18)
+                .addComponent(lblNome1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(txtNome, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(cmbTipo)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(lblLogin)
+                .addComponent(lblNome, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(txtNome)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(txtValor, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(36, 36, 36)
+                .addComponent(lblLogin, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(txtValor)
+                .addGap(20, 20, 20)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(btnCancelar, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btnSalvar, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(btnSalvar, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnCancelar, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(20, 20, 20))
         );
 
         pack();
@@ -99,24 +114,39 @@ public class CadProdutoForm extends javax.swing.JInternalFrame {
     }//GEN-LAST:event_btnCancelarActionPerformed
 
     private void btnSalvarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSalvarActionPerformed
-        Produto prod = new Produto();
-        prod.setNome(txtNome.getText().trim());
 
-        BigDecimal valor = new BigDecimal(txtValor.getText());
-        prod.setValor(valor);
-
-        ProdutoBO prodBO = new ProdutoBO();
         try {
-            prodBO.Salvar(prod);
-            JOptionPane.showMessageDialog(null,
-                    "Cadastro realizado com sucesso!",
-                    "Cadastrar Produto",
-                    JOptionPane.INFORMATION_MESSAGE);
-            
-            this.Limpar();
+            if ((txtNome.getText().equals("")
+                    || txtValor.getText().equals("")
+                    || cmbTipo.getSelectedItem().equals("Selecione um Opção"))) {
 
+                JOptionPane.showMessageDialog(null,
+                        "É obrigatorio preencher ou selecionar todos os campos!",
+                        "Cadastrar Produto",
+                        JOptionPane.ERROR_MESSAGE);
+
+            } else {
+
+                Produto prod = new Produto();
+                prod.setNome(txtNome.getText().trim());
+                BigDecimal valor = new BigDecimal(txtValor.getText());
+                prod.setValor(valor);
+                prod.setTipo(cmbTipo.getSelectedItem().toString());
+                ProdutoBO prodBO = new ProdutoBO();
+                prodBO.Salvar(prod);
+
+                JOptionPane.showMessageDialog(null,
+                        "Cadastro realizado com sucesso!",
+                        "Cadastrar Produto",
+                        JOptionPane.INFORMATION_MESSAGE);
+
+                this.Limpar();
+            }
         } catch (Exception ex) {
-
+            JOptionPane.showMessageDialog(null,
+                    "É obrigatorio preencher o valor e Utilize ponto no lugar da virgula no valor do Produto!",
+                    "Cadastrar Produto",
+                    JOptionPane.ERROR_MESSAGE);
         }
 
 
@@ -125,13 +155,16 @@ public class CadProdutoForm extends javax.swing.JInternalFrame {
     public void Limpar() {
         txtNome.setText("");
         txtValor.setText("");
+        cmbTipo.setSelectedIndex(0);
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnCancelar;
     private javax.swing.JButton btnSalvar;
+    private javax.swing.JComboBox cmbTipo;
     private javax.swing.JLabel lblLogin;
     private javax.swing.JLabel lblNome;
+    private javax.swing.JLabel lblNome1;
     private javax.swing.JTextField txtNome;
     private javax.swing.JTextField txtValor;
     // End of variables declaration//GEN-END:variables
