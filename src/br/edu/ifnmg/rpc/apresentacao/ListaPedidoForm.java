b0a7620ecb4f22461328b6896e40f9bfb865d6af
ALUNO 2015-1 <ALUNO 2015-1@JAN-LB7-COM-030>
2016-01-11 10:27:25 -0200

    Alterações

--- src/br/edu/ifnmg/rpc/apresentacao/ListaPedidoForm.java
+++ src/br/edu/ifnmg/rpc/apresentacao/ListaPedidoForm.java
@@ -11,6 +11,7 @@ import java.awt.Dimension;
 import java.util.List;
 import javax.swing.JDesktopPane;
 import javax.swing.JInternalFrame;
+import javax.swing.JOptionPane;
 import javax.swing.table.AbstractTableModel;
 
 /**
@@ -19,7 +20,8 @@ import javax.swing.table.AbstractTableModel;
  */
 public class ListaPedidoForm extends javax.swing.JInternalFrame {
 
-     JDesktopPane JDP1 = TelaPrincipal.getPainel(null);
+    JDesktopPane JDP1 = TelaPrincipal.getPainel(null);
+
     /**
      * Creates new form CadPedidoForm
      */
@@ -42,6 +44,7 @@ public class ListaPedidoForm extends javax.swing.JInternalFrame {
         tblPedidos = new javax.swing.JTable();
         btnNovoPedido = new javax.swing.JButton();
         btnFinalizarPedido = new javax.swing.JButton();
+        add = new javax.swing.JButton();
 
         setClosable(true);
         setMaximizable(true);
@@ -68,18 +71,18 @@ public class ListaPedidoForm extends javax.swing.JInternalFrame {
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jScrollPane2)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 891, Short.MAX_VALUE)
                 .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 510, Short.MAX_VALUE)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 525, Short.MAX_VALUE)
                 .addContainerGap())
         );
 
-        btnNovoPedido.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/edu/ifnmg/rpc/icones/add-icon3232.png"))); // NOI18N
+        btnNovoPedido.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/edu/ifnmg/rpc/icones/tab_new.png"))); // NOI18N
         btnNovoPedido.setText("Novo Pedido");
         btnNovoPedido.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -89,6 +92,14 @@ public class ListaPedidoForm extends javax.swing.JInternalFrame {
 
         btnFinalizarPedido.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/edu/ifnmg/rpc/icones/register-icon.png"))); // NOI18N
         btnFinalizarPedido.setText("Finalizar Pedido");
+
+        add.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/edu/ifnmg/rpc/icones/add-icon3232.png"))); // NOI18N
+        add.setText("Adicionar Itens");
+        add.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                addActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -100,7 +111,9 @@ public class ListaPedidoForm extends javax.swing.JInternalFrame {
                     .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(btnNovoPedido)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 651, Short.MAX_VALUE)
+                        .addGap(45, 45, 45)
+                        .addComponent(add, javax.swing.GroupLayout.PREFERRED_SIZE, 149, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(btnFinalizarPedido)))
                 .addContainerGap())
         );
@@ -110,8 +123,9 @@ public class ListaPedidoForm extends javax.swing.JInternalFrame {
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(btnNovoPedido)
-                    .addComponent(btnFinalizarPedido))
-                .addGap(18, 18, 18)
+                    .addComponent(btnFinalizarPedido)
+                    .addComponent(add, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addContainerGap())
         );
@@ -127,7 +141,24 @@ public class ListaPedidoForm extends javax.swing.JInternalFrame {
         novopedido.toFront();
         JDP1.add(novopedido);
     }//GEN-LAST:event_btnNovoPedidoActionPerformed
-   private void ConfigurarTblPedido() {
+
+    private void addActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_addActionPerformed
+        int seleciona = tblPedidos.getSelectedRow();
+        if (seleciona >= 0) {
+            String codigo = tblPedidos.getModel().getValueAt(seleciona, 0).toString();
+            String nome = tblPedidos.getModel().getValueAt(seleciona, 1).toString();
+            this.dispose();
+            AddItemPedidoForm addItemPedido = new AddItemPedidoForm(codigo);
+            addItemPedido.setVisible(true);
+            centralizaForm(addItemPedido);
+            addItemPedido.toFront();
+            JDP1.add(addItemPedido);
+
+        } else {
+            JOptionPane.showMessageDialog(null, "Selecione uma linha antes de adicionar!", "Adicionar Itens ao Pedido", JOptionPane.INFORMATION_MESSAGE);
+        }
+    }//GEN-LAST:event_addActionPerformed
+    private void ConfigurarTblPedido() {
         TablePedidoModel model = new TablePedidoModel(new PedidoDAO().Buscar());
         tblPedidos.setModel(model);
     }
@@ -140,19 +171,16 @@ public class ListaPedidoForm extends javax.swing.JInternalFrame {
             this.lista = lista;
         }
 
-        
         @Override
         public int getRowCount() {
             return lista.size();
         }
 
-        
         @Override
         public int getColumnCount() {
             return 2;
         }
 
-       
         @Override
         public Object getValueAt(int rowIndex, int columnIndex) {
             Pedido pedido = lista.get(rowIndex);
@@ -164,18 +192,18 @@ public class ListaPedidoForm extends javax.swing.JInternalFrame {
             return null;
         }
 
-       
         @Override
         public String getColumnName(int column) {
-            if(column == 0){
+            if (column == 0) {
                 return "Codigo";
-            }else if(column == 1){
+            } else if (column == 1) {
                 return "Nome";
             }
             return null;
         }
 
     }
+
     private void centralizaForm(JInternalFrame frame) {
 
         Dimension desktopSize = JDP1.getSize();
@@ -186,6 +214,7 @@ public class ListaPedidoForm extends javax.swing.JInternalFrame {
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton add;
     private javax.swing.JButton btnFinalizarPedido;
     private javax.swing.JButton btnNovoPedido;
     private javax.swing.JPanel jPanel1;
